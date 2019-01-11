7625780ff64ca6b1041a2689e68743e513b564ad
kongliangzhong <kongliangzhong@gmail.com>
2019-01-11 11:29:52 +0800

    prepare for lightcone docker image

--- migrations/4_deploy_protocol.js
+++ migrations/4_deploy_protocol.js
@@ -9,45 +9,74 @@ var OrderBook = artifacts.require("./impl/OrderBook");
 var BurnRateTable = artifacts.require("./impl/BurnRateTable");
 var BurnManager = artifacts.require("./impl/BurnManager");
 var LRCToken = artifacts.require("./test/tokens/LRC.sol");
+var GTOToken = artifacts.require("./test/tokens/GTO.sol");
 var WETHToken = artifacts.require("./test/tokens/WETH.sol");
 
-module.exports = function(deployer, network, accounts) {
+module.exports = async function(deployer, network, accounts) {
 
   if (network === "live") {
     // ignore.
   } else {
-    deployer.then(() => {
-      return Promise.all([
-        TradeDelegate.deployed(),
-        TradeHistory.deployed(),
-        BrokerRegistry.deployed(),
-        OrderRegistry.deployed(),
-        FeeHolder.deployed(),
-        OrderBook.deployed(),
-        LRCToken.deployed(),
-        WETHToken.deployed(),
-      ]);
-    }).then(() => {
-      return deployer.deploy(BurnRateTable, LRCToken.address, WETHToken.address);
-    }).then(() => {
-      return Promise.all([
-        deployer.deploy(
-          RingSubmitter,
-          LRCToken.address,
-          WETHToken.address,
-          TradeDelegate.address,
-          TradeHistory.address,
-          BrokerRegistry.address,
-          OrderRegistry.address,
-          FeeHolder.address,
-          OrderBook.address,
-          BurnRateTable.address,
-        ),
-        deployer.deploy(OrderCanceller, TradeHistory.address),
-        deployer.deploy(BurnManager, FeeHolder.address, LRCToken.address),
-      ]);
-    }).then(() => {
-      // do nothing
-    });
+    const [
+      delegate,
+      history,
+    ] = await Promise.all([
+      TradeDelegate.deployed(),
+      TradeHistory.deployed(),
+      BrokerRegistry.deployed(),
+      OrderRegistry.deployed(),
+      FeeHolder.deployed(),
+      OrderBook.deployed(),
+    ]);
+
+    // test tokens:
+    const tokens = await Promise.all([
+      LRCToken.deployed(),
+      WETHToken.deployed(),
+      GTOToken.deployed(),
+    ]);      
+
+    await deployer.deploy(BurnRateTable, LRCToken.address, WETHToken.address);
+    await deployer.deploy(
+      RingSubmitter,
+      LRCToken.address,
+      WETHToken.address,
+      TradeDelegate.address,
+      TradeHistory.address,
+      BrokerRegistry.address,
+      OrderRegistry.address,
+      FeeHolder.address,
+      OrderBook.address,
+      BurnRateTable.address,
+    );
+    await deployer.deploy(OrderCanceller, TradeHistory.address);
+    await deployer.deploy(BurnManager, FeeHolder.address, LRCToken.address);
+
+    // do authorize:
+    await delegate.authorizeAddress(RingSubmitter.address, {from: accounts[0]});
+    await history.authorizeAddress(RingSubmitter.address, {from: accounts[0]});
+    await history.authorizeAddress(OrderCanceller.address, {from: accounts[0]});
+
+    // set balance and approve.
+    const maxValue = "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff";
+    for (const t of tokens) {
+      await t.setBalance(accounts[0], maxValue);
+      await t.approve(TradeDelegate.address, maxValue, {from: accounts[0]});
+    }
+
+    // print addresses:
+    console.log("LRCToken.address", LRCToken.address);
+    console.log("WETHToken.address",WETHToken.address);
+    console.log("GTO.address",GTOToken.address);
+    console.log("TradeDelegate.address",TradeDelegate.address);
+    console.log("TradeHistory.address",TradeHistory.address);
+    console.log("BrokerRegistry.address",BrokerRegistry.address);
+    console.log("OrderRegistry.address",OrderRegistry.address);
+    console.log("FeeHolder.address",FeeHolder.address);
+    console.log("OrderBook.address",OrderBook.address);
+    console.log("BurnRateTable.address",BurnRateTable.address);
+    console.log("RingSubmitter.address",RingSubmitter.address);
+    console.log("OrderCanceller.address",OrderCanceller.address);
+    console.log("BurnManager.address",BurnManager.address);
   }
 };
